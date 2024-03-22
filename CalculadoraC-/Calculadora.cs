c0f1eda1c120a7674358e8d20016f1f39671f566
unknown <ruan.silva149@etec.sp.gov>
2024-03-21 22:21:23 -0300

    calculadora

---
+++ CalculadoraC-/Calculadora.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraC_
+{
+    internal class Calculadora
+    {
+        public int soma()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = num1 + num2;
+
+            Console.WriteLine("Resultado: {0}", result);
+
+            return result;
+        }
+        public int subtracao()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = num1 - num2;
+
+            Console.WriteLine("Resultado: {0}", result);
+
+            return result;
+
+        }
+        public int multiplicacao()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = num1 * num2;
+
+            Console.WriteLine("Resultado: {0}", result);
+
+            return result;
+        }
+        public double divisao()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            while (num1 == 0)
+            {
+                Console.WriteLine("Erro: não é possível dividir 0");
+                return 0;
+            }
+
+            double result = num1 / num2;
+
+            Console.WriteLine("Resultado: {0}", result);
+
+            return result;
+
+        }
+    }
+}
+
---
+++ CalculadoraC-/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora");
+
+            Calculadora calculadora = new Calculadora();
+
+            
+            Console.WriteLine("Digite a operação: ");
+            string operacao = Console.ReadLine();
+            switch (operacao)
+                {
+
+                    case "+":
+                        calculadora.soma();
+                        break;
+
+                    case "-":
+                        calculadora.subtracao();
+                        break;
+
+                    case "*":
+                        calculadora.multiplicacao();
+                        break;
+
+                    case "/":
+                        calculadora.divisao();
+                        break;
+                }
+            Console.ReadKey(); 
+
+        }
+    }
+}
+       
